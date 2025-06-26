4793d332c4c4c1fd632ce234e9c20c1a9de0bbd7
Mahmoud Alhijjiri <mahmoudalhijjiri@Mahmouds-Mac-mini.local>
2025-06-26 23:25:44 +0200

    ✨ Add: Service layers for contract and event logic
    - Created services/contract_service.py with contract-related database operations
    - Created services/event_service.py to handle event-related queries and updates
    - Prepared structure for future CLI refactoring and improved modularity

--- Epic_events/cli/__init__.py
+++ Epic_events/cli/__init__.py
@@ -1,5 +1,5 @@
 import click
-from .user import register_user, login, whoami
+from .user import register_user, login, whoami, list_users
 from .client import register_client, list_clients, list_my_clients
 
 
@@ -13,6 +13,7 @@ def cli():
 cli.add_command(register_user, name="register_user")
 cli.add_command(login, name="login")
 cli.add_command(whoami, name="whoami")
+cli.add_command(list_users, name="list_users")
 
 # Register subcommands to CLI (clients)
 cli.add_command(register_client, name="register_client")
